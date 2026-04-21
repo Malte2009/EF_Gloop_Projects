4e95f401ccfb7b9a45acf53569aa3a5a5c5b0647
Malte Roesgen <malte.roesgen@gmail.com>
2026-04-21 09:25:51 +0200

    remove files

--- src/de/kag/langenfeld/ef/heli/Starter.java
+++
@@ -1,9 +0,0 @@
-package de.kag.langenfeld.ef.heli;
-
-public class Starter {
-
-    public static void main(String[] args) {
-        Landeplatzszene landeplatz = new Landeplatzszene();
-        landeplatz.starte();
-    }
-}
